dad32f96bcbec121759e43d07ea252bbbb5a46f7
Yaroslav Valentynskyy <sukrnet@gmail.com>
2016-07-07 16:58:03 +0200

    JUnit tests fix

--- src/test/java/XORundTest.java
+++ src/test/java/XORundTest.java
@@ -5,10 +5,9 @@ public class XORundTest {
     @Test
     public void rundTest() throws Exception{
         Przedszkolak p=new Przedszkolak("s","a");
-        int k;
-        for(k=0;k<100;k++){
+        for(int k=0;k<8;k++){
             k=p.rund(0,7);
-            assertTrue(k>=0 && k<=7);
+            assertTrue(k>-1 && k<8);
         }
     }
 }
